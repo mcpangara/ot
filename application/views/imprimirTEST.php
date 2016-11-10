85a15455e988ad3825d7302f351fb52a2046ef7b
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-10 15:06:40 -0500

    dopdf

--- application/views/imprimirTEST.php
+++ application/views/imprimirTEST.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-    <style media="screen">
-      body { font-family: DejaVu Sans; }
-      td, th{
-        border: 1px solid #333;
-      }
-    </style>
-
     <h1>DomPDF</h1>
 
     <table>
-        <tr>
-          <th>Test</th>
-          <th>Test</th>
-          <th>Test</th>
-          <th>Test</th>
-        </tr>
         <?php
         for ($i=0; $i < 20; $i++) {
         ?>
@@ -35,6 +14,3 @@
         }
         ?>
     </table>
-
-  </body>
-</html>
