e6ba982994b794d325dcb6b142370188e336d4a4
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-10 16:33:45 -0500

    dompdf

--- application/views/imprimirTEST.php
+++ application/views/imprimirTEST.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
     <h1>DomPDF</h1>
 
     <table>
@@ -20,3 +27,5 @@
         </tr>
       </tbody>
     </table>
+  </body>
+</html>
