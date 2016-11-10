4006ba260b047234d4ea8f6c608ae0ef94bcd335
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-10 07:55:51 -0500

    ot imp

--- application/controllers/Ot.php
+++ application/controllers/Ot.php
@@ -184,7 +184,8 @@ class Ot extends CI_Controller {
 			'tr'=>$tr
 		);
 		$html = $this->load->view('ot/imprimir/formatoOT',$data,TRUE);
-		doPDF($html, $ot->nombre_ot);
+		echo $html;
+		//doPDF($html, $ot->nombre_ot);
 	}
 
 	public function getMyReportes($value='')
