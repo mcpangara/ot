57c6e8c80d189a9ea5cf9372cb2e5c0976097dcf
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-10 09:38:32 -0500

    asd

--- application/views/imprimirTEST.php
+++ application/views/imprimirTEST.php
@@ -11,8 +11,7 @@
       td, th{
         overflow: visible;
         height: 4ex;
-        width:auto;
-        min-width: 10ex;
+        width: 10ex;
         border: 1px solid #333;
       }
     </style>
