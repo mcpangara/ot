be5832d890d3db92017856995a3d3a2963e7d92b
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-16 18:07:27 -0500

    OT

--- application/config/database.php
+++ application/config/database.php
@@ -95,28 +95,6 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-$db['ot'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'termo.sistemas.apps',
-	'database' => 'ot',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
 /*
 $db['ot'] = array(
 	'dsn'	=> '',
@@ -140,3 +118,25 @@ $db['ot'] = array(
 	'save_queries' => TRUE
 );
 */
+
+$db['ot'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'mysql',
+	'database' => 'ot',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
