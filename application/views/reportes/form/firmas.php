9a685feebd89039486958cf8cffe124e94a2eca7
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-17 12:33:19 -0500

    mejores

---
+++ application/views/reportes/form/firmas.php
@@ -0,0 +1,23 @@
+  <section>
+    <table>
+      <thead>
+        <tr>
+          <th>ELABORADOR POR:</th>
+          <th>REPRESENTANTE CONTREATISTA</th>
+          <th>REPRESENTANTE ECOPETROL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nombre: <input type="text" ng-model="rd.firmas.elaborador_nombre" value=""> </td>
+          <td>Nombre: <input type="text" ng-model="rd.firmas.contratista_nombre" value=""> </td>
+          <td>Nombre: <input type="text" ng-model="rd.firmas.ecopetrol_nombre" value=""> </td>
+        </tr>
+        <tr>
+          <td>Cargo: <input type="text" ng-model="rd.firmas.elaborado_cargo" value=""> </td>
+          <td>Cargo: <input type="text" ng-model="rd.firmas.contratista_cargo" value=""> </td>
+          <td>Cargo: <input type="text" ng-model="rd.firmas.ecopetrol_cargo" value=""> </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
