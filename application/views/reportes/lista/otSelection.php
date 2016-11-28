a0aaa0da7358c205080ad41d4c0f3aac8b0aaa6d
Yeison <yeisontorrado@termotecnica.com.co>
2016-11-28 11:02:32 -0500

    Mejoras selección de OT para Reportes diarios 12.1

--- application/views/reportes/lista/otSelection.php
+++ application/views/reportes/lista/otSelection.php
@@ -1,8 +1,8 @@
-<section id="seleccionar-ot" class="nodisplay" style="position: absolute;  padding: 2ex; box-shadow: 0px 0px 5px #333; z-index:3; background:#FEFEFE">
+<section id="seleccionar-ot" class="nodisplay col s12 m12 l12" style="max-width:90%; position: relative; margin: 1ex; box-shadow: 0px 0px 5px #333; z-index:3; background: #F5F5F5; height:200px; overflow: auto">
   <h5>Seleciona la OT buscada</h5>
   <hr>
   <div class="">
-    <table class="mytabla">
+    <table class="mytabla" style="background:#FFF">
       <thead>
         <tr>
           <th>Seleccionar</th>
@@ -12,7 +12,7 @@
       <tbody>
         <tr ng-repeat="ot in myOts">
           <td>
-            <button type="button" class="btn" ng-click="seleccionarOT(ot)"></button>
+            <button type="button" class="btn mini-btn2" ng-click="seleccionarOT(ot)">Seleccionar</button>
           </td>
           <td ng-bind="ot.nombre_ot">
           </td>
