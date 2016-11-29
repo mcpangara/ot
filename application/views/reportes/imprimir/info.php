d9962d4a8c19f49860cb7f4e0f14e29baacf5207
Yeison Torrado López <tristan.herth@gmail.com>
2016-11-29 02:42:18 -0500

    inicio formato imprimir RD

--- application/views/reportes/imprimir/info.php
+++ application/views/reportes/imprimir/info.php
@@ -1,8 +1,8 @@
 <table>
   <thead>
     <tr>
-      <th colspan="3"> <img src="<?= base_url('assets/img/termotecnica.jpg') ?>" style="max-width: 150px" alt=""> </th>
-      <th colspan="7">REPORTE DIARIO DE TIEMPO TRABAJADO Y ACTIVIDADES DE OBRA EJECUTADA</th>
+      <th colspan="2"> <img src="<?= base_url('assets/img/termotecnica.jpg') ?>" style="max-width: 150px" alt=""> </th>
+      <th colspan="4">REPORTE DIARIO DE TIEMPO TRABAJADO Y ACTIVIDADES DE OBRA EJECUTADA</th>
     </tr>
   </thead>
   <tbody>
@@ -12,23 +12,34 @@
         EJECUCIÓN DE OBRAS Y TRABAJOS DE MANTENIMIENTO DE SISTEMAS DE TRANSPORTE DE HIDROCARBUROS
       </td>
       <td colspan="3">
-
-        <table>
-          <thead>
-            <tr>
-              <th>DD</th>
-              <th>MM</th>
-              <th>AA</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><?= date('d', strtotime($r->fecha_reporte)) ?></td>
-            </tr>
-          </tbody>
-        </table>
-
+          <table>
+            <thead>
+              <tr>
+                <th>DD</th>
+                <th>MM</th>
+                <th>AA</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><?= date('d', strtotime($r->fecha_reporte)) ?></td>
+                <td><?= date('m', strtotime($r->fecha_reporte)) ?></td>
+                <td><?= date('Y', strtotime($r->fecha_reporte)) ?></td>
+              </tr>
+            </tbody>
+          </table>
       </td>
+      <td>Día: </td>
+    </tr>
+
+    <tr>
+      <td colspan="2">
+        O.T. No.:
+        <div class="">
+          <?= $r->nombre_ot ?>
+        </div>
+      </td>
+
     </tr>
   </tbody>
 </table>
